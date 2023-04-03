1ec78372be160a00450cfb34b878a9d7c0f1ade7
杨飞 <philyang7@outlook.com>
2023-04-03 11:46:17 +0800

    change text

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -210,7 +210,7 @@ export default () => {
         fallback={() => (
           <div class="gen-cb-wrapper">
             <span>AI is thinking...</span>
-            <div class="gen-cb-stop" onClick={stopStreamFetch}>中止</div>
+            <div class="gen-cb-stop" onClick={stopStreamFetch}>Stop</div>
           </div>
         )}
       >
@@ -230,7 +230,7 @@ export default () => {
             class="gen-textarea"
           />
           <button onClick={handleButtonClick} disabled={systemRoleEditing()} gen-slate-btn>
-            发送
+          Send
           </button>
           <button title="Clear" onClick={clear} disabled={systemRoleEditing()} gen-slate-btn>
             <IconClear />
